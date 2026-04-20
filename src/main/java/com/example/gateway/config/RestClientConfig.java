b04d8b415ebd91f33a0d25215e5666a24a1bf601
algaaa <anikanova.aa@phystech.edu>
2026-04-20 21:42:55 +0300

    feat: add clients for part 2B

---
+++ src/main/java/com/example/gateway/config/RestClientConfig.java
@@ -0,0 +1,33 @@
+package com.example.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${external.api.base-url}")
+    private String baseUrl;
+
+    @Bean
+    public RestClient restClient() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(2))
+                .build();
+
+        JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
+        requestFactory.setReadTimeout(Duration.ofSeconds(3));
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .baseUrl(baseUrl)
+                .defaultHeader("User-Agent")
+                .build();
+    }
+}
